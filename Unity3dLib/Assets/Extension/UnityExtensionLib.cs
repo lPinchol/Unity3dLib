4e5e6706cd26f71aa5a26c180b78994ea33eb8d2
lPinchol <pinchodev91@gmail.com>
2015-05-12 22:38:29 +0200

    Prepare estructure base :+1:

---
+++ Unity3dLib/Assets/Extension/UnityExtensionLib.cs
@@ -0,0 +1,17 @@
+﻿//*******************************************************
+//  Autor:          Antonio Mateo "lPinchol"
+//  Description:    Help for Unity3D
+//  Date:           12/05/2015
+//  GitHub:         https://github.com/lPinchol
+//******************************************************
+
+using UnityEngine;
+using System.Collections;
+
+namespace UnityNerve
+{
+    public class UnityExtensionLib : MonoBehaviour
+    {
+    }
+}
+
